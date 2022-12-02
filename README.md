36f8365a9ef5f3d89173a36bf565dad312a18bcf
Eddie <48199805+UnitedPuggs@users.noreply.github.com>
2022-12-02 13:52:55 -0800

    feat: links to admin sites, wip: most recent searches

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
+# SupportSearch
 A chrome extension tool to make it easier to find Clubspeed-related information!
-
-## Allowing for quick retrieval of site information!
-![](https://github.com/UnitedPuggs/SupportSearch/supporty.gif)
-![](https://github.com/UnitedPuggs/SupportSearch/blob/main/supporty.gif))
--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 let search = document.getElementById("license");
 let searchButton = document.getElementById("searchButton");
 
+let mostRecentSearches = []
+
 async function licenseLookup(e) {
     if(e.key === 'Enter' || e.type == "click") {
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -20,19 +22,26 @@ if(search) {
     searchButton.addEventListener("click", licenseLookup);
 }
 
+function addLinks(url) {
+    document.getElementById("versioninfo").innerHTML += "<a href='http://" + url + "/sp_admin' target='_blank'>sp_admin</a>";
+    document.getElementById("versioninfo").innerHTML += "<br><a href='http://" + url + "/admin' target='_blank'>admin</a></div>";
+}
+
 //Just so it continues on in the promise after 500ms in case the server is down
 const control = new AbortController();
-const timeout = setTimeout(() => control.abort(), 500);
+const timeout = setTimeout(() => control.abort(), 50);
 
+/*** DATABASE RELATED FUNCTIONS ***/
 //Makes a GET request to see if the licenseName is in MySQL db
 function findLicenseName(licenseName) {
-    fetch("http://35.215.125.37/receive_license.php?licenseName=" + licenseName, { signal: control.signal })
+    fetch(("http://35.215.125.37/receive_license.php?licenseName=" + licenseName), { signal: control.signal })
     .then(response =>  response.json())
     .then(data => { 
         if(data.length == 0) {
             getCSTimingVers(licenseName);
         } else {
-            document.getElementById("versioninfo").innerHTML = "<div>Version:</div>" + data[0].VersionNumber + "<br>" + data[0].LastUpdated;
+            document.getElementById("versioninfo").innerHTML = "<div>Version: " + data[0].VersionNumber + "<br>Last Updated: " + data[0].LastUpdated.substring(0, 10) + "<br>Links:";
+            addLinks(data[0].LicenseName);
         }
     })
     .catch(err => {
@@ -53,14 +62,22 @@ function insertLicenseName(licenseName, versionNumber, lastUpdated) {
     })
     .catch(err => console.log(err))
 }
+/***********************************/
 
+//*** INTERNAL API FUNCTIONS ***/
 //Makes an API call for CS timing URL with the license name
 function getCSTimingVers(licenseName) {
     fetch("https://" + licenseName + ".clubspeedtiming.com/api/index.php/version/current.json?key=cs-dev")
         .then(response => response.json())
         .then(data => {
-            document.getElementById("versioninfo").innerHTML = "<div>Version:</div>" + data.CurrentVersion + "<br>" + data.LastUpdated; 
-            insertLicenseName(licenseName, data.CurrentVersion, data.LastUpdated); 
+            document.getElementById("versioninfo").innerHTML = "<div>Version: " + data.CurrentVersion + "<br>Last Updated: " + data.LastUpdated.substring(0, 10) + "<br>Links:"; 
+            addLinks((licenseName + ".clubspeedtiming.com"));
+            insertLicenseName((licenseName + ".clubspeedtiming.com"), data.CurrentVersion, data.LastUpdated);
+            if(Object.keys(mostRecentSearches).length == 3) {
+
+            } else {
+
+            }
         })
         .catch(err => { 
             if(err.message.includes("Failed to fetch")) { 
@@ -74,11 +91,13 @@ function getCSVers(licenseName) {
     fetch("https://" + licenseName + ".clubspeed.com/api/index.php/version/current.json?key=cs-dev")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("versioninfo").innerHTML = "<div>Version:</div>" + data.CurrentVersion + "<br>" + data.LastUpdated; 
-        insertLicenseName(licenseName, data.CurrentVersion, data.LastUpdated); 
+        document.getElementById("versioninfo").innerHTML = "<div>Version: " + data.CurrentVersion + "<br>Last Updated: " + data.LastUpdated.substring(0, 10) + "<br>Links:"; 
+        addLinks((licenseName + ".clubspeed.com"));
+        insertLicenseName((licenseName + ".clubspeed.com"), data.CurrentVersion, data.LastUpdated); 
     })
     .catch(err => { 
         console.log(err); 
         document.getElementById("versioninfo").innerHTML = "No response received. Check network connectivity of this site."; 
     });
 }
+//********************/
